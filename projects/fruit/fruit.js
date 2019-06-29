6264ceadae1a12e8246b8676bc9d2ee9095eecec
samanthaeileen1 <samchristensen5@gmail.com>
2019-06-28 22:31:15 -0600

    more options

--- projects/fruit/fruit.js
+++ projects/fruit/fruit.js
@@ -17,6 +17,42 @@ function setFruit() {
         document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
     }
 
+    if (fruit === "orange" || fruit === "Orange" || fruit === "oranges" || fruit === "Oranges" ) {
+        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+    }
+
+    if (fruit === "pear" || fruit === "Pear" || fruit === "pears" || fruit === "Pears" ) {
+        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+    }
+
+    if (fruit === "peach" || fruit === "Peach" || fruit === "Peaches" || fruit === "peaches" ) {
+        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+    }
+
+    if (fruit === "cherries" || fruit === "Cherries") {
+        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+    }
+
+    if (fruit === "pineapple" || fruit === "Pineapple" || fruit === "pineapples" || fruit === "Pineapples" ) {
+        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+    }
+
+    if (fruit === "raspberries" || fruit === "Raspberries") {
+        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+    }
+
+    if (fruit === "blueberries" || fruit === "Blueberries") {
+        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+    }
+
+    if (fruit === "stawberries" || fruit === "Strawberries") {
+        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+    }
+
+    if (fruit === "mango" || fruit === "Mango" || fruit === "Mangos" || fruit === "mangos" ) {
+        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+    }
+
     
     }
     
