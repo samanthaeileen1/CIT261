32b063e0da4853c4fc06d9973334765aea14ac98
samanthaeileen1 <samchristensen5@gmail.com>
2019-06-28 19:03:06 -0600

    try

--- projects/businessWebsite/businessWebsite.js
+++ projects/businessWebsite/businessWebsite.js
@@ -5,34 +5,28 @@ for giving me a start on this. Especially with the crazy math stuff
 
 function makeHeart() {
 
-var heart = document.getElementById("heart");
-var context = heart.getContext("2d");
-/*dimensions*/
-    context.x = 150;
-    context.y = 150;
-    var width  = 400;
-    var height = 400;
-    /*styles*/
-    context.strokeStyle = "#B3CD61";
-    context.strokeWeight = 2;
-    
-    /*not sure*/
-    context.shadowOffsetX = 4.0;
-    context.shadowOffsetY = 4.0;
-    context.lineWidth = 10.0;
-    /*don't know if this works as a word instead of numbers*/
-    context.fillStyle = "pink";
-    var d = Math.min(width, height);
-    var k = 120;
-    context.moveTo(k, k + d / 4);
-    context.quadraticCurveTo(k, k, k + d / 4, k);
-    context.quadraticCurveTo(k + d / 2, k, k + d / 2, k + d / 4);
-    context.quadraticCurveTo(k + d / 2, k, k + d * 3/4, k);
-    context.quadraticCurveTo(k + d, k, k + d, k + d / 4);
-    context.quadraticCurveTo(k + d, k + d / 2, k + d * 3/4, k + d * 3/4);
-    context.lineTo(k + d / 2, k + d);
-    context.lineTo(k + d / 4, k + d * 3/4);
-    context.quadraticCurveTo(k, k + d / 2, k, k + d / 4);
-    context.stroke();
-    context.fill();
+canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+canvas.x = 150;
+canvas.y = 150;
+var w = 200, h = 200;
+context.strokeStyle = "#000000";
+context.strokeWeight = 3;
+context.shadowOffsetX = 4.0;
+context.shadowOffsetY = 4.0;
+context.lineWidth = 10.0;
+context.fillStyle = "#FF0000";
+var d = Math.min(w, h);
+var k = 120;
+context.moveTo(k, k + d / 4);
+context.quadraticCurveTo(k, k, k + d / 4, k);
+context.quadraticCurveTo(k + d / 2, k, k + d / 2, k + d / 4);
+context.quadraticCurveTo(k + d / 2, k, k + d * 3/4, k);
+context.quadraticCurveTo(k + d, k, k + d, k + d / 4);
+context.quadraticCurveTo(k + d, k + d / 2, k + d * 3/4, k + d * 3/4);
+context.lineTo(k + d / 2, k + d);
+context.lineTo(k + d / 4, k + d * 3/4);
+context.quadraticCurveTo(k, k + d / 2, k, k + d / 4);
+context.stroke();
+context.fill();
 }
