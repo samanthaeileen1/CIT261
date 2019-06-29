0d934f8d6b18b61657318b8a5a4aa69a3ecb39d3
samanthaeileen1 <samchristensen5@gmail.com>
2019-06-28 23:59:18 -0600

    all fruit profiles

--- projects/fruit/fruit.js
+++ projects/fruit/fruit.js
@@ -23,64 +23,74 @@ function setFruit() {
     if (fruit === "orange" || fruit === "Orange" || fruit === "oranges" || fruit === "Oranges" ) {
         document.getElementById("picture").innerHTML = "<img src='orange.jpg' height='480' width='600'>";
         var orange = new Fruit("orange", true, "trees", false);
+        orange.display();
     }
 
     if (fruit === "apple" || fruit === "Apple" || fruit === "Apples" || fruit === "apples" ) {
         document.getElementById("picture").innerHTML = "<img src='apple.jpg' height='480' width='480'>";
         var apple = new Fruit("red, green, yellow", false, "trees", false);
+        apple.display();
     }
     
 
     if (fruit === "pear" || fruit === "Pear" || fruit === "pears" || fruit === "Pears" ) {
         document.getElementById("picture").innerHTML = "<img src='pear.jpg' height='480' width='480'>";
         var pear = new Fruit("green, red, brown", false, "trees", false);
+        pear.display();
     }
 
     if (fruit === "peach" || fruit === "Peach" || fruit === "Peaches" || fruit === "peaches" ) {
         document.getElementById("picture").innerHTML = "<img src='peach.jpg' height='480' width='480'>";
         var peach = new Fruit("orange, yellow", false, "trees", false);
+        peach.display();
     }
 
     if (fruit === "cherries" || fruit === "Cherries") {
         document.getElementById("picture").innerHTML = "<img src='cherries.jpg' height='480' width='480'>";
         var cherry = new Fruit("red, purple", false, "trees", false);
-
+        cherry.display();
     }
 
     if (fruit === "pineapple" || fruit === "Pineapple" || fruit === "pineapples" || fruit === "Pineapples" ) {
         document.getElementById("picture").innerHTML = "<img src='pineapple.jpg' height='480' width='480'>";
         var pineapple = new Fruit("yellow", true, "trees", false);
+        pineapple.display();
     }
 
     if (fruit === "raspberries" || fruit === "Raspberries") {
         document.getElementById("picture").innerHTML = "<img src='raspberries.jpg' height='480' width='510'>";
         var raspberry = new Fruit("red", false, "bushes", true);
+        raspberry.display();
     }
 
     if (fruit === "blueberries" || fruit === "Blueberries") {
         document.getElementById("picture").innerHTML = "<img src='blueberries.jpg' height='480' width='480'>";
         var blueberry = new Fruit("blue", false, "bushes", true);
+        blueberry.display();
     }
 
     if (fruit === "blackberries" || fruit === "Blackberries") {
         document.getElementById("picture").innerHTML = "<img src='blackberries.jpg' height='480' width='530'>";
         var blackberry = new Fruit("blackish purplely blue", false, "bushes", true);
+        blackberry.display();
     }
 
     if (fruit === "strawberries" || fruit === "Strawberries") {
         document.getElementById("picture").innerHTML = "<img src='strawberries.jpg' height='480' width='530'>";
         var strawberries = new Fruit("red", false, "bushes", true);
+        strawberries.display();
     }
 
     if (fruit === "mango" || fruit === "Mango" || fruit === "Mangoes" || fruit === "mangoes" ) {
         document.getElementById("picture").innerHTML = "<img src='mango.jpg' height='480' width='530'>";
         var mango = new Fruit("orange, yellow, red, green", true, "trees", false);
+        mango.display();
     }
 
     if (fruit === "grape" || fruit === "Grape" || fruit === "grapes" || fruit === "Grapes" ) {
         document.getElementById("picture").innerHTML = "<img src='grapes.jpg' height='480' width='530'>"
         var grape = new Fruit("green, purple", false, "vine", true);
-        
+        grape.display();
     }
 
 
