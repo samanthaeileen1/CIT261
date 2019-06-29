a315b4501f634e0a236ec4bdc84970e777eb74ae
samanthaeileen1 <samchristensen5@gmail.com>
2019-06-28 22:37:14 -0600

    more fruit choices

--- projects/fruit/fruit.js
+++ projects/fruit/fruit.js
@@ -18,42 +18,54 @@ function setFruit() {
     }
 
     if (fruit === "orange" || fruit === "Orange" || fruit === "oranges" || fruit === "Oranges" ) {
-        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+        document.getElementById("picture").innerHTML = "<img src='bananas.jpg' height='480' width='480'>"
+    }
+
+    if (fruit === "apple" || fruit === "Apple" || fruit === "Apples" || fruit === "apples" ) {
+        document.getElementById("picture").innerHTML = "<img src='apple.jpg' height='480' width='480'>"
     }
 
     if (fruit === "pear" || fruit === "Pear" || fruit === "pears" || fruit === "Pears" ) {
-        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+        document.getElementById("picture").innerHTML = "<img src='pear.jpg' height='480' width='480'>"
     }
 
     if (fruit === "peach" || fruit === "Peach" || fruit === "Peaches" || fruit === "peaches" ) {
-        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+        document.getElementById("picture").innerHTML = "<img src='peach.jpg' height='480' width='480'>"
     }
 
     if (fruit === "cherries" || fruit === "Cherries") {
-        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+        document.getElementById("picture").innerHTML = "<img src='cherries.jpg' height='480' width='480'>"
     }
 
     if (fruit === "pineapple" || fruit === "Pineapple" || fruit === "pineapples" || fruit === "Pineapples" ) {
-        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+        document.getElementById("picture").innerHTML = "<img src='pineapple.jpg' height='480' width='480'>"
     }
 
     if (fruit === "raspberries" || fruit === "Raspberries") {
-        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+        document.getElementById("picture").innerHTML = "<img src='raspberries.jpg' height='480' width='480'>"
     }
 
     if (fruit === "blueberries" || fruit === "Blueberries") {
-        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+        document.getElementById("picture").innerHTML = "<img src='blueberries.jpg' height='480' width='480'>"
+    }
+
+    if (fruit === "blackberries" || fruit === "Blackberries") {
+        document.getElementById("picture").innerHTML = "<img src='blackberries.jpg' height='480' width='480'>"
     }
 
     if (fruit === "stawberries" || fruit === "Strawberries") {
-        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+        document.getElementById("picture").innerHTML = "<img src='strawberries.jpg' height='480' width='480'>"
     }
 
-    if (fruit === "mango" || fruit === "Mango" || fruit === "Mangos" || fruit === "mangos" ) {
-        document.getElementById("picture").innerHTML = "<img src='bananas.jpeg' height='480' width='480'>"
+    if (fruit === "mango" || fruit === "Mango" || fruit === "Mangoes" || fruit === "mangoes" ) {
+        document.getElementById("picture").innerHTML = "<img src='mango.jpg' height='480' width='480'>"
     }
 
-    
+    if (fruit === "grape" || fruit === "Grape" || fruit === "grapes" || fruit === "Grapes" ) {
+        document.getElementById("picture").innerHTML = "<img src='grapes.jpg' height='480' width='480'>"
+    }
+
+
     }
     
     function removeFruit() {
