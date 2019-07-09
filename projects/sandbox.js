2361ff2fb9ca61a0866fa4164570bd4070754cba
samanthaeileen1 <samchristensen5@gmail.com>
2019-07-09 11:22:18 -0600

    fixed it

--- projects/sandbox.js
+++ projects/sandbox.js
@@ -1,24 +1,20 @@
-function makeRequest(){
-// Create a request variable and assign a new XMLHttpRequest object to it.
+function makeRequest() {
+
 var request = new XMLHttpRequest()
 
-// Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-
-request.onload = function () {
+request.onload = function() {
   // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      console.log(movie.title)
+    })
+  } else {
+    console.log('error')
   }
-
-// Send request
-request.send()
-
-// Begin accessing JSON data here
-var data = JSON.parse(this.response)
-
-data.forEach(movie => {
-  // Log each movie's title
-  console.log(movie.title)
-})
-
 }
 
+request.send()
+}
