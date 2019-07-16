9e4167729d37e19ec4edc7fd72e751ef1326b431
samanthaeileen1 <samchristensen5@gmail.com>
2019-07-16 13:52:47 -0600

    hehe

--- projects/maybeBaby/maybeBaby.js
+++ projects/maybeBaby/maybeBaby.js
@@ -1,14 +1,3 @@
-function getType() {
-    var type1 = document.getElementById("type").value;
-    return type1;
-}
-
-function getDate() {
-    var date = document.getElementById("date").value;
-    return date;
-}
-
-
 function setDate() {
     document.getElementById("div1").innerHTML = document.getElementById("date").value;
 }
